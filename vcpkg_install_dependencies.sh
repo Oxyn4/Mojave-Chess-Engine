e99df42186cecb42ddbe79941c36f347b7cb9e9c
jacob <jacob4evans434@gmail.com>
2023-03-17 23:34:30 +0000

    added vcpkg install script

---
+++ vcpkg_install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone https://github.com/Microsoft/vcpkg.git
+
+./vcpkg/bootstrap-vcpkg.sh
+
+cmake -B build/ -S . -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
+
+
